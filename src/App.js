404d70004baed8852bd1e7ba83c0331a10a47488
vicky-angular <69048391+vicky-angular@users.noreply.github.com>
2024-11-24 19:23:11 +0530

    same f (#2)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js </code> and save to reload.
         </p>
         <p>This is new <b>Feature</b><h1>Dropdown</h1></p>
+        <p>This is new <b>Feature</b><i>HEADING</i>yes featrue -1</p>
         <p>This is new <b>Feature</b><i>HEADING</i> works</p>
         <a
           className="App-link"
