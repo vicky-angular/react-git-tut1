b0d0f72001c6c6dcc5a052557f031ecaaf422fae
Vivek <vivekrana1947@gmail.com>
2024-11-24 19:06:28 +0530

    resolve merge conflicts

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js </code> and save to reload.
         </p>
-        <p>This is new <b>Feature</b></p>
+        <p>This is new <b>Feature</b><i>HEADING</i></p>
         <a
           className="App-link"
           href="https://reactjs.org"
